05f5f9025402cb559a49c17a17010752a6924853
francesco.malagisi <francesco.malagisi@accenture.com>
2021-12-30 16:36:14 +0100

    remove math util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './array.util';
 export * from './currency.util';
 export * from './keys.util';
-export * from './math.util';
 export * from './crypto.util';
 export * from './color.util';
--- src/utils/math.util.ts
+++
@@ -1,5 +0,0 @@
-import { subtract, multiply } from 'ramda';
-
-export const multiplication = (multiplicand: number, multiplier: number): number => multiply(multiplicand, multiplier);
-
-export const subtraction = (firstNumber: number, secondNumber: number): number => subtract(firstNumber, secondNumber);
